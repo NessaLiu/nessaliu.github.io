4409e94f232080ab02d1f6da62d9e65ac4631fd2
NessaLiu <liu_vanessa@rocketmail.com>
2022-01-17 23:38:31 -0500

    size fix

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import "./banner.css"
 const Banner = () => {
     return (
         <section id="banner">
-            <div className="banner-container">
+            <div className="banner-container grid">
                 <div className="banner-bg"></div>
                 <h1 className="banner-name">V A N E S S A • L I U</h1>
             </div>
