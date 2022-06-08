1c9a6b5742e026c1b3cb754b574e514242b79d39
NessaLiu <liu_vanessa@rocketmail.com>
2022-06-08 14:26:43 -0400

    fixed interests responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       <Quote />
       <Resume />
       <Projects />
-      {/* <Interests/> */}
+      <Interests />
       {/* <CarouselGallery/> */}
       {/* <Contact/> */}
       {/* <Footer/> */}
