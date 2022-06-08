85b822dd524b12792d8932f8c89ffb9d46d91877
NessaLiu <liu_vanessa@rocketmail.com>
2022-06-08 12:21:59 -0400

    fixed intro responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <div>
       <Navbar/>
       <Banner/>
-      {/* <Intro/> */}
+      <Intro/>
       {/* <Quote/> */}
       {/* <Resume/> */}
       {/* <Projects/> */}
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,9 +6,6 @@ const Banner = () => {
         <section id="banner">
             <div className="banner-container">
                 <h1 className="banner-name">V A N E S S A • L I U</h1>
-                <svg className="banner-bg" src={bannerBg} alt="banner-background" />
-                
-                {/* <div className="banner-bg"></div> */}
             </div>
         </section>
     )
