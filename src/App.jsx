0486f58dd98b12366a27874f6151838c78656489
NessaLiu <liu_vanessa@rocketmail.com>
2022-06-07 22:33:54 -0400

    feature: editing banner responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,14 @@ const App = () => {
     <div>
       <Navbar/>
       <Banner/>
-      <Intro/>
-      <Quote/>
-      <Resume/>
-      <Projects/>
-      <Interests/>
-      <CarouselGallery/>
-      <Contact/>
-      <Footer/>
+      {/* <Intro/> */}
+      {/* <Quote/> */}
+      {/* <Resume/> */}
+      {/* <Projects/> */}
+      {/* <Interests/> */}
+      {/* <CarouselGallery/> */}
+      {/* <Contact/> */}
+      {/* <Footer/> */}
     </div>
   );
 };
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,14 @@
 import "./banner.css"
+import bannerBg from '../../images/banner-bg.svg';
 
 const Banner = () => {
     return (
         <section id="banner">
-            <div className="banner-container grid">
-                <div className="banner-bg"></div>
+            <div className="banner-container">
                 <h1 className="banner-name">V A N E S S A • L I U</h1>
+                <svg className="banner-bg" src={bannerBg} alt="banner-background" />
+                
+                {/* <div className="banner-bg"></div> */}
             </div>
         </section>
     )
