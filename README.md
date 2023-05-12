c94436a993bdcbd062d3826b8ef42959f0ff9d1b
NessaLiu <liu_vanessa@rocketmail.com>
2023-05-11 23:54:14 -0400

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Vanessa Liu's Personal Website
-
-Welcome to my personal website! I've documented past projects and experiences I've worked on, while also playing around with front-end technologies :)
-
--- src/data/eases.js
+++
@@ -1,8 +0,0 @@
-import { gsap } from 'gsap';
-import { CustomEase } from "gsap/CustomEase";
-gsap.registerPlugin(CustomEase);
-
-CustomEase.create(
-    "easeBounce",
-    "M0,0 C0.46,0 0.28,1.044 0.486,1.232 0.69,1.418 0.674,0.394 1,1 "
-);
